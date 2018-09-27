be01ba692d276fbdc62c0bc2b233c80957f66b0f
高橋延明 <TakahashiNobuaki@takahashinobemei-no-MacBook-Air.local>
2018-09-27 21:23:38 +0900

    [ここに変更点を書く]

---
+++ kadai02/detail.php
@@ -0,0 +1,68 @@
+<?php
+// getで送信されたidを取得
+$id = $_GET['id'];
+echo "GET:".$id;
+
+
+//1.  DB接続します
+include('functions.php');
+ $pdo=db_conn();
+
+
+//２．データ登録SQL作成，指定したidのみ表示する
+$stmt = $pdo->prepare('SELECT * FROM gs_user_table WHERE id=:id');
+$stmt->bindValue(':id',$id, PDO::PARAM_INT);
+$status = $stmt->execute();
+
+//３．データ表示
+if($status==false){
+  // エラーのとき
+  errorMsg($stmt);
+}else{
+  // エラーでないとき
+  $rs = $stmt->fetch();
+  // fetch()で1レコードを取得して$rsに入れる
+  // $rsは配列で返ってくる．$rs["id"], $rs["name"]などで値をとれる
+  // var_dump()で見てみよう
+}
+?>
+
+<!-- htmlは「index.php」とほぼ同じです -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>更新ページ</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body>
+
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+    <div class="navbar-header"><a class="navbar-brand" href="select.php">データ一覧</a></div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<form method="post" action="update.php">
+  <div class="jumbotron">
+   <fieldset>
+    <legend>更新ページ</legend>
+     <label>名前：<input type="text" name="name" value="<?=$rs['name']?>"></label><br>
+     <label>ID：<input type="text" name="lid" value="<?=$rs['lid']?>"></label><br>
+     <label>PW：<input type="text" name="lpw" value="<?=$rs['lpw']?>"></label><br>
+     <input type="submit" value="送信">
+     <!-- idは変えたくない = ユーザーから見えないようにする-->
+     <input type="hidden" name="id" value="<?=$rs['id']?>">
+    </fieldset>
+  </div>
+</form>
+<!-- Main[End] -->
+
+
+</body>
+</html>
---
+++ kadai02/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>ブックマークアプリユーザー管理</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body>
+
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+    <div class="navbar-header"><a class="navbar-brand" href="select.php">データ一覧</a></div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<form method="post" action="insert.php">
+  <div class="jumbotron">
+   <fieldset>
+    <legend>本のブックマーク</legend>
+     <label>ユーザー名：<input type="text" name="name"></label><br>
+     <label>ID：<input type="text" name="lid"></label><br>
+     <label>PW：<input type="text" name="lpw"></label><br>
+     <input type="submit" value="送信">
+    </fieldset>
+  </div>
+</form>
+<!-- Main[End] -->
+
+
+</body>
+</html>
